e17366809d704e2431ae8839c07c914a7bb28924
guga-coelho <gualvescoelho@gmail.com>
2023-04-17 10:40:27 -0300

    Layout almost ready and button

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,35 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora de IMC"),
+        title: const Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+        ],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Icon(Icons.person_outlined, size: 120, color: Colors.green),
+          entrada_texto("Peso em KG"),
+          entrada_texto("Altura em CM"),
+          ElevatedButton(onPressed: () {}, child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25),), style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => Colors.green))),
+          Text("Info",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.green, fontSize: 25)
+          )
+        ],
+      )
     );
+  }
+
+  TextField entrada_texto(String label) {
+    return  TextField(keyboardType: TextInputType.number, decoration: InputDecoration(labelText: label,
+          labelStyle: const TextStyle(color: Colors.green)),  
+          textAlign: TextAlign.center,
+          style: const TextStyle(color: Colors.green, fontSize: 25)        
+          );
   }
 }

ddec771a2ee09d9e07daa96d492427b7d5adc61c
guga-coelho <gualvescoelho@gmail.com>
2023-04-17 11:08:30 -0300

    concluindo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  String _infoText = "Informe seus dados";
+
+  void resetFields() {
+    pesoController.text = '';
+    alturaController.text = '';
+    setState(() {
+      _infoText = "Informe seus dados";
+    });
+  }
+
+  void calculate() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+
+      _infoText = imc < 18.6 ? "Abaixo do peso(${imc.toStringAsPrecision(4)})" : 'Você não está abaixo do peso';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,31 +44,35 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+          IconButton(onPressed: resetFields, icon: const Icon(Icons.refresh))
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           const Icon(Icons.person_outlined, size: 120, color: Colors.green),
-          entrada_texto("Peso em KG"),
-          entrada_texto("Altura em CM"),
-          ElevatedButton(onPressed: () {}, child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25),), style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => Colors.green))),
-          Text("Info",
+          entrada_texto("Peso em KG", pesoController),
+          entrada_texto("Altura em CM", alturaController),
+          ElevatedButton(onPressed: calculate, child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 25),), style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => Colors.green))),
+          Text(_infoText,
           textAlign: TextAlign.center,
           style: TextStyle(color: Colors.green, fontSize: 25)
           )
         ],
       )
+      )
     );
   }
 
-  TextField entrada_texto(String label) {
+  TextField entrada_texto(String label, TextEditingController controller) {
     return  TextField(keyboardType: TextInputType.number, decoration: InputDecoration(labelText: label,
           labelStyle: const TextStyle(color: Colors.green)),  
           textAlign: TextAlign.center,
-          style: const TextStyle(color: Colors.green, fontSize: 25)        
+          style: const TextStyle(color: Colors.green, fontSize: 25),
+          controller: controller,        
           );
   }
 }
